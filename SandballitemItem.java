bbad50597ccd52e848a27180eb77bed17aef6f54
SkyNotTheLimit <159592458+ekulxam@users.noreply.github.com>
2024-02-23 11:01:21 -0800

    Add files via upload

---
+++ SandballitemItem.java
@@ -0,0 +1,60 @@
+package name.modid.item.custom;
+
+import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.Multimap;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Vanishable;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.stat.Stats;
+import net.minecraft.util.Hand;
+import net.minecraft.util.Rarity;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.UseAction;
+import net.minecraft.world.World;
+
+
+public class SandballitemItem extends Item {
+    private final Multimap<EntityAttribute, EntityAttributeModifier> attributeModifiers;
+    public Multimap<EntityAttribute, EntityAttributeModifier> getAttributeModifiers(EquipmentSlot slot) {
+        return slot == EquipmentSlot.MAINHAND ? this.attributeModifiers : super.getAttributeModifiers(slot);
+    }
+    public SandballitemItem(Settings settings) {
+        super(settings);
+        ImmutableMultimap.Builder<EntityAttribute, EntityAttributeModifier> builder = ImmutableMultimap.builder();
+        builder.put(EntityAttributes.GENERIC_ATTACK_DAMAGE, new EntityAttributeModifier(ATTACK_DAMAGE_MODIFIER_ID, "Tool modifier", 4.0, EntityAttributeModifier.Operation.ADDITION));
+        builder.put(EntityAttributes.GENERIC_ATTACK_SPEED, new EntityAttributeModifier(ATTACK_SPEED_MODIFIER_ID, "Tool modifier", -2.4, EntityAttributeModifier.Operation.ADDITION));
+        this.attributeModifiers = builder.build();
+    }
+    @Override
+    public UseAction getUseAction(ItemStack itemstack) {
+        return UseAction.SPEAR;
+    }
+    @Override
+    public int getMaxUseTime(ItemStack itemstack) {
+        return 20;
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        ItemStack stack = user.getStackInHand(hand);
+        if (!world.isClient() && user instanceof ServerPlayerEntity player) {
+            // add ONE line to shoot your custom projectile
+            if (!player.getAbilities().creativeMode) {
+                stack.decrement(1);
+                if (stack.isEmpty())
+                    user.incrementStat(Stats.USED.getOrCreateStat(this));
+            }
+        }
+        return TypedActionResult.success(stack, world.isClient());
+    }
+}
