ef7de2d90519574e8d2030c3fbd55d903304533d
SkyNotTheLimit <159592458+ekulxam@users.noreply.github.com>
2025-01-26 09:31:53 -0800

    Add HeldItemRendererMixin for doublegauntlet

--- doublegauntlet/mixin/HeldItemRendererMixin.java
+++ doublegauntlet/mixin/HeldItemRendererMixin.java
@@ -1,1 +1,19 @@
+package survivalblock.renderbothgauntlet.mixin;
 
+import net.minecraft.client.render.item.HeldItemRenderer;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+@Mixin(HeldItemRenderer.class)
+public class HeldItemRendererMixin {
+
+    @Shadow private ItemStack mainHand;
+
+    @ModifyArg(method = "renderItem(FLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider$Immediate;Lnet/minecraft/client/network/ClientPlayerEntity;I)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/item/HeldItemRenderer;renderFirstPersonItem(Lnet/minecraft/client/network/AbstractClientPlayerEntity;FFLnet/minecraft/util/Hand;FLnet/minecraft/item/ItemStack;FLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", ordinal = 1), index = 5)
+    private ItemStack renderBothGaunletsInFirstPerson(ItemStack item) {
+        return this.mainHand.isOf(/* item */) ? this.mainHand.copy() : item;
+    }
+}
